0eca9a86540cdd640cb4d4a8d1a6e4ff0cdc1483
Pedro Gomes <37028775+pedro-gomes-92@users.noreply.github.com>
2018-04-21 00:45:14 +0100

    Added environments to use proxy only on devmode

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class MessageService {
-
-	private static DATA_PATH = 'data/denni_kurz.txt';
-
 	constructor(private http: Http) {
 
 	}
 
 	public processMessage(...args: Array<Object>): any {
 		return this.http.get(
-			`${MessageService.DATA_PATH}?${args.map((arg: Object) => { return `${arg['name']}=${arg['value']}`; }).join('&')}`,
+			`${environment.DATA_PATH}/denni_kurz.txt?${args.map((arg: Object) => { return `${arg['name']}=${arg['value']}`; }).join('&')}`,
 			'{responseType: "text"}');
 	}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  DATA_PATH: 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu'
 };

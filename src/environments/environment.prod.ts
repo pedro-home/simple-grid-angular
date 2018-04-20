db253227cdfc3c8488611071124e7bb1c6ece964
Pedro Gomes <37028775+pedro-gomes-92@users.noreply.github.com>
2018-04-21 00:53:01 +0100

    Added environments to use proxy only on devmode (II)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  DATA_PATH: 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu'
+  DATA_PATH: 'https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu'
 };
